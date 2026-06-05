441bc3792f951d3ba98748fcfbf705bfd47e6da4
Mark Ivory <114793567+MarkIvory2973@users.noreply.github.com>
2026-06-05 22:16:59 +0800

    perf: optimize syncer

--- src/internal/core/syncer.go
+++ src/internal/core/syncer.go
@@ -7,29 +7,34 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
+	"sync"
 )
 
 func SyncDirs(srcRoot string, srcDirs []string, dstRoot string, dstDirs []string) {
+	var waitGroup sync.WaitGroup
+
 	for _, srcDir := range srcDirs {
 		if slices.Contains(dstDirs, srcDir) {
 			continue
 		}
 
-		srcPath := filepath.Join(srcRoot, srcDir)
-		srcStat, err := os.Stat(srcPath)
-		if err != nil {
-			fmt.Printf("warning: unable to visit '%s': %v\n", srcPath, err)
-			continue
-		}
+		waitGroup.Go(func() {
+			srcPath := filepath.Join(srcRoot, srcDir)
+			srcStat, err := os.Stat(srcPath)
+			if err != nil {
+				fmt.Printf("warning: unable to visit '%s': %v\n", srcPath, err)
+				return
+			}
 
-		newPath := filepath.Join(dstRoot, srcDir)
-		err = os.MkdirAll(newPath, srcStat.Mode().Perm())
-		if err != nil {
-			fmt.Printf("warning: unable to create '%s': %v\n", newPath, err)
-			continue
-		}
+			newPath := filepath.Join(dstRoot, srcDir)
+			err = os.MkdirAll(newPath, srcStat.Mode().Perm())
+			if err != nil {
+				fmt.Printf("warning: unable to create '%s': %v\n", newPath, err)
+				return
+			}
 
-		fmt.Printf("+ '%s'\n", srcDir)
+			fmt.Printf("+ '%s'\n", srcDir)
+		})
 	}
 
 	for _, dstDir := range dstDirs {
@@ -37,32 +42,40 @@ func SyncDirs(srcRoot string, srcDirs []string, dstRoot string, dstDirs []string
 			continue
 		}
 
-		delPath := filepath.Join(dstRoot, dstDir)
-		err := os.RemoveAll(delPath)
-		if err != nil {
-			fmt.Printf("warning: unable to remove '%s': %v\n", delPath, err)
-			continue
-		}
+		waitGroup.Go(func() {
+			delPath := filepath.Join(dstRoot, dstDir)
+			err := os.RemoveAll(delPath)
+			if err != nil {
+				fmt.Printf("warning: unable to remove '%s': %v\n", delPath, err)
+				return
+			}
 
-		fmt.Printf("- '%s'\n", dstDir)
+			fmt.Printf("- '%s'\n", dstDir)
+		})
 	}
+
+	waitGroup.Wait()
 }
 
 func SyncFiles(srcRoot string, srcFiles []string, dstRoot string, dstFiles []string) {
+	var waitGroup sync.WaitGroup
+
 	for _, srcFile := range srcFiles {
 		if slices.Contains(dstFiles, srcFile) {
 			continue
 		}
 
-		srcPath := filepath.Join(srcRoot, srcFile)
-		dstPath := filepath.Join(dstRoot, srcFile)
-		err := files.Copy(srcPath, dstPath)
-		if err != nil {
-			fmt.Printf("warning: unable to copy '%s': %v\n", srcFile, err)
-			continue
-		}
+		waitGroup.Go(func() {
+			srcPath := filepath.Join(srcRoot, srcFile)
+			dstPath := filepath.Join(dstRoot, srcFile)
+			err := files.Copy(srcPath, dstPath)
+			if err != nil {
+				fmt.Printf("warning: unable to copy '%s': %v\n", srcFile, err)
+				return
+			}
 
-		fmt.Printf("+ '%s'\n", srcFile)
+			fmt.Printf("+ '%s'\n", srcFile)
+		})
 	}
 
 	for _, dstFile := range dstFiles {
@@ -70,14 +83,16 @@ func SyncFiles(srcRoot string, srcFiles []string, dstRoot string, dstFiles []str
 			continue
 		}
 
-		delPath := filepath.Join(dstRoot, dstFile)
-		err := os.Remove(delPath)
-		if err != nil {
-			fmt.Printf("warning: unable to remove '%s': %v\n", dstFile, err)
-			continue
-		}
+		waitGroup.Go(func() {
+			delPath := filepath.Join(dstRoot, dstFile)
+			err := os.Remove(delPath)
+			if err != nil {
+				fmt.Printf("warning: unable to remove '%s': %v\n", dstFile, err)
+				return
+			}
 
-		fmt.Printf("- '%s'\n", dstFile)
+			fmt.Printf("- '%s'\n", dstFile)
+		})
 	}
 
 	for _, srcFile := range srcFiles {
@@ -85,30 +100,34 @@ func SyncFiles(srcRoot string, srcFiles []string, dstRoot string, dstFiles []str
 			continue
 		}
 
-		srcPath := filepath.Join(srcRoot, srcFile)
-		srcChecksum, err := files.Checksum(srcPath)
-		if err != nil {
-			fmt.Printf("warning: unable to checksum '%s': %v\n", srcFile, err)
-			continue
-		}
+		waitGroup.Go(func() {
+			srcPath := filepath.Join(srcRoot, srcFile)
+			srcChecksum, err := files.Checksum(srcPath)
+			if err != nil {
+				fmt.Printf("warning: unable to checksum '%s': %v\n", srcFile, err)
+				return
+			}
 
-		dstPath := filepath.Join(dstRoot, srcFile)
-		dstChecksum, err := files.Checksum(dstPath)
-		if err != nil {
-			fmt.Printf("warning: unable to checksum '%s': %v\n", srcFile, err)
-			continue
-		}
+			dstPath := filepath.Join(dstRoot, srcFile)
+			dstChecksum, err := files.Checksum(dstPath)
+			if err != nil {
+				fmt.Printf("warning: unable to checksum '%s': %v\n", srcFile, err)
+				return
+			}
 
-		if bytes.Equal(srcChecksum, dstChecksum) {
-			continue
-		}
+			if bytes.Equal(srcChecksum, dstChecksum) {
+				return
+			}
 
-		err = files.Copy(srcPath, dstPath)
-		if err != nil {
-			fmt.Printf("warning: unable to update '%s': %v\n", srcFile, err)
-			continue
-		}
+			err = files.Copy(srcPath, dstPath)
+			if err != nil {
+				fmt.Printf("warning: unable to update '%s': %v\n", srcFile, err)
+				return
+			}
 
-		fmt.Printf("M '%s'\n", srcFile)
+			fmt.Printf("M '%s'\n", srcFile)
+		})
 	}
+
+	waitGroup.Wait()
 }
